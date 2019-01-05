dec1601bb20cf21ff7e2d3fb3b916f61ef83db42
xiao <903772703@qq.com>
2019-01-05 17:01:09 +0800

    ,,,

---
+++ src/test7/Test7_13.java
@@ -0,0 +1,29 @@
+package test7;
+
+class Ab{
+	void f(int i) {
+		System.out.println("i");
+	}
+	void f(char i) {
+		System.out.println("c");
+	}
+	void f(float i) {
+		System.out.println("f");
+	}
+}
+public class Test7_13 extends Ab{
+	void f(String i) {
+		System.out.println("s");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test7_13 t = new Test7_13();
+		t.f(2);
+		t.f('a');
+		t.f(12.23f);
+		t.f("hello");
+
+	}
+
+}
---
+++ src/test7/Test7_5.java
@@ -0,0 +1,20 @@
+package test7;
+
+
+class A{
+	A(){
+		System.out.println("a");
+}
+}
+class B{
+	B(){		
+		System.out.println("b");
+	}
+}
+public class Test7_5 extends A {
+	B b;
+	public static void main(String[] args) {
+		Test7_5 t = new Test7_5();
+	}
+
+}
